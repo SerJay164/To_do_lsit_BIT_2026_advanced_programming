0c4623c55a4b0da50ac6089ed88c0661a5ea2cb1
JustinVfhnw <justin.vogler@students.fhnw.ch>
2026-05-14 23:45:24 +0200

    updated/corrected database.py

--- database.py
+++ database.py
@@ -1,23 +1,25 @@
 import sqlite3
 
-DB_NAME = to_do.db
+DB_NAME = "to_do.db"
+
 
 def create_connection():
     return sqlite3.connect(DB_NAME)
+
 
 def create_tables():
     conn = create_connection()
     cursor = conn.cursor()
 
-    cursor.execute('''
-    CREATE TABLE IF NOT EXISTS tasks (
-        id INTEGER PRIMARY KEY,
-        title TEXT NOT NULL,
-        due_date TEXT,
-        status TEXT,
-        priority TEXT,
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS tasks (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            title TEXT NOT NULL,
+            due_date TEXT,
+            status TEXT NOT NULL DEFAULT 'pending',
+            priority TEXT NOT NULL
         )
-        ''')
-    
+    """)
+
     conn.commit()
     conn.close()
